4a0a05b2b88abc2cdf6eceaca068d722fe7e3985
Uriel Rodriguez <u.e.rodriguez@gmail.com>
2020-01-19 18:07:29 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -6,6 +6,7 @@ PAT_M =  { name:  "Pat Monteseian", occupation: "Staff" }
 KATE_G = { name:  "Kate Gompert", occupation: "None" }
 BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
 
+
 def assembled_aoh
   
   people = [
